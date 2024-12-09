e4e4a9615743a6afc380ee738abdf330c73b7297
Alec B <Alexander.Barry@keyin.com>
2024-12-09 09:42:42 -0330

    Commented-out non-existent page routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Order from "./pages/Order";
-import Customize from "./pages/Customize";
-import OurStory from "./pages/OurStory";
+// import Customize from "./pages/Customize";
+// import OurStory from "./pages/OurStory";
 
 const App = () => {
   return (
@@ -16,8 +16,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/order" element={<Order />} />
-          <Route path="/customize" element={<Customize />} />
-          <Route path="/our-story" element={<OurStory />} />
+          {/* <Route path="/customize" element={<Customize />} />
+          <Route path="/our-story" element={<OurStory />} /> */}
         </Routes>
 
         <Footer />
